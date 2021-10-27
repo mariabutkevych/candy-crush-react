d3a758ab5900b26492100fc7b6a78522910996a6
marymmme <mashabut@ukr.net>
2021-10-27 22:07:35 +0300

    Checking for matches

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const candyColors = [
     'purple',
     'red',
     'yellow'
-]
+];
 
 
 const App = () => {
@@ -17,7 +17,7 @@ const App = () => {
 
     const checkForColumnOfFour = () => {
         for (let i = 0; i < 39; i++) {
-            const columnOfFour = [i, i + width, width * 3 ];
+            const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
             const decidedColor = currentColorArrangement[i];
 
             if (columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
@@ -28,7 +28,7 @@ const App = () => {
 
     const checkForColumnOfThree = () => {
         for (let i = 0; i < 47; i++) {
-            const columnOfThree = [i, i + width, width * 2 ];
+            const columnOfThree = [i, i + width, i + width * 2];
             const decidedColor = currentColorArrangement[i];
 
             if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
@@ -37,6 +37,62 @@ const App = () => {
         }
     };
 
+    const checkForRowOfThree = () => {
+        for (let i = 0; i < 64; i++) {
+            const rowOfThree = [i, i + 1, i + 2];
+            const decidedColor = currentColorArrangement[i];
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 38, 39, 46, 47, 54, 55, 63, 64];
+
+            if (notValid.includes(i)) continue;
+
+            if (rowOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+                rowOfThree.forEach(square => currentColorArrangement[square] = '')
+            }
+        }
+    };
+
+    const checkForRowOfFour = () => {
+        for (let i = 0; i < 64; i++) {
+            const rowOfFour = [i, i + 1, i + 2, i + 3];
+            const decidedColor = currentColorArrangement[i];
+            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 30, 31, 37, 38, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64];
+
+            if (notValid.includes(i)) continue;
+
+            if (rowOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+                rowOfFour.forEach(square => currentColorArrangement[square] = '')
+            }
+        }
+    };
+
+    const moveIntoSquareBelow = () => {
+        for (let i = 0; i < 64 - width; i++) {
+            const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
+            const isFirstRow = firstRow.includes(i);
+            const randomNumber = Math.floor(Math.random() * candyColors.length);
+
+            if (isFirstRow && currentColorArrangement[i] === '') {
+                currentColorArrangement[i] = candyColors[randomNumber];
+            }
+
+            if (currentColorArrangement[i + width] === '') {
+                currentColorArrangement[i + width] = currentColorArrangement[i];
+                currentColorArrangement[i] = '';
+            }
+        }
+    };
+
+    const dragStart = () => {
+        console.log('drag start');
+    };
+
+    const dragDrop = () => {
+        console.log('drag drop');
+    };
+
+    const dragEnd = () => {
+        console.log('drag end');
+    };
 
     const createBoard = () => {
         const randomColorArrangement = [];
@@ -57,11 +113,14 @@ const App = () => {
     useEffect(() => {
         const timer = setInterval(() => {
             checkForColumnOfFour();
+            checkForRowOfFour();
             checkForColumnOfThree();
+            checkForRowOfThree();
+            moveIntoSquareBelow();
             setCurrentColorArrangement([...currentColorArrangement]);
-        }, 100)
+        }, 100);
         return () => clearInterval(timer);
-    }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement]);
+    }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, moveIntoSquareBelow, currentColorArrangement]);
 
   return (
     <div className="app">
@@ -71,11 +130,19 @@ const App = () => {
                     key={index}
                     style={{backgroundColor: candyColor}}
                     alt={candyColor}
+                    data-id={index}
+                    draggable={true}
+                    onDragStart={dragStart}
+                    onDragOver={(e) => e.preventDefault()}
+                    onDragEnter={(e) => e.preventDefault()}
+                    onDragLeave={(e) => e.preventDefault()}
+                    onDrop={dragDrop}
+                    onDragEnd={dragEnd}
                 />
             ))}
         </div>
     </div>
   );
-}
+};
 
 export default App;
